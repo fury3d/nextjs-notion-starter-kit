80190bb973869969c86612533098487fddcd63b9
fury3d <55603292+fury3d@users.noreply.github.com>
2026-07-22 19:13:30 -0300

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -1,6 +1,42 @@
+import type * as types from 'notion-types'
+import cs from 'classnames'
 import * as React from 'react'
+import { Breadcrumbs, Search } from 'react-notion-x'
 
-export const NotionPageHeader = () => {
+import { isSearchEnabled } from '@/lib/config'
+import { MoonIcon } from '@/lib/icons/moon'
+import { SunIcon } from '@/lib/icons/sun'
+import { useDarkMode } from '@/lib/use-dark-mode'
+
+import styles from './styles.module.css'
+
+function ToggleThemeButton() {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  const onToggleTheme = React.useCallback(() => {
+    toggleDarkMode()
+  }, [toggleDarkMode])
+
+  return (
+    <div
+      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+      onClick={onToggleTheme}
+    >
+      {hasMounted && isDarkMode ? <MoonIcon /> : <SunIcon />}
+    </div>
+  )
+}
+
+export function NotionPageHeader({
+  block
+}: {
+  block: types.CollectionViewPageBlock | types.PageBlock
+}) {
   return (
     <header style={{
       display: 'flex',
@@ -8,35 +44,36 @@ export const NotionPageHeader = () => {
       alignItems: 'center',
       width: '100%',
       padding: '12px 24px',
-      backgroundColor: '#2f3438', // Cor escura atual
+      backgroundColor: '#2f3438',
       borderBottom: '1px solid #3a3f42'
     }}>
       {/* Esquerda: Logo */}
       <img src="/vivaicon2.png" alt="Logo" style={{ height: '50px', objectFit: 'contain' }} />
 
-      {/* Centro: Título */}
-      <h1 style={{ 
-        color: '#00E5C5', 
-        fontSize: '22px', 
-        fontWeight: '900', 
-        margin: 0,
-        fontFamily: 'sans-serif',
-        textTransform: 'uppercase',
-        textAlign: 'center'
+      {/* Centro: Título Dinâmico do Notion */}
+      <div style={{
+        color: '#00E5C5',
+        fontSize: '20px',
+        fontWeight: '900',
+        textTransform: 'uppercase'
       }}>
-        PORTAL DA TRANSPARÊNCIA
-      </h1>
+        <Breadcrumbs block={block} />
+      </div>
 
-      {/* Direita: Slogan */}
-      <div style={{ 
-        textAlign: 'right', 
-        fontWeight: '900', 
-        fontSize: '11px', 
-        color: '#ffffff',
-        lineHeight: '1.1',
-        fontFamily: 'sans-serif'
-      }}>
-        A GENTE<br />CUIDA<br />DO QUE É<br />NOSSO!
+      {/* Direita: Busca, Tema e Slogan */}
+      <div style={{ display: 'flex', alignItems: 'center', gap: '15px' }}>
+        {isSearchEnabled && <Search block={block} />}
+        <ToggleThemeButton />
+        <div style={{
+          textAlign: 'right',
+          fontWeight: '900',
+          fontSize: '11px',
+          color: '#ffffff',
+          lineHeight: '1.1',
+          fontFamily: 'sans-serif'
+        }}>
+          A GENTE<br />CUIDA<br />DO QUE É<br />NOSSO!
+        </div>
       </div>
     </header>
   )
